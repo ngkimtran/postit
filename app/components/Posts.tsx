c2a12480a26f9966f5a040d6f420977d5911b3f9
Kim Tran <ngockimtranle@gmail.com>
2023-03-15 15:11:33 +0200

    Restructure Posts component

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -3,7 +3,8 @@
 import React from "react";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
-import SinglePost from "./SinglePost";
+import Image from "next/image";
+import Link from "next/link";
 import { PostType } from "../types/Posts";
 
 const getAllPosts = async () => {
@@ -23,14 +24,28 @@ const Posts = () => {
   return (
     <>
       {data?.map((post) => (
-        <SinglePost
-          key={post.id}
-          name={post.user.name}
-          avatar={post.user.image}
-          title={post.title}
-          id={post.id}
-          comments={post.comments}
-        />
+        <div key={post.id} className="bg-white my-8 p-8 rounded-lg">
+          <div className="flex items-center gap-2">
+            <Image
+              className="rounded-full"
+              width={32}
+              height={32}
+              src={post.user.image}
+              alt="avatar"
+            />
+            <h3 className="font-bold text-gray-700">{post.user.name}</h3>
+          </div>
+          <div className="my-8">
+            <p className="break-all">{post.title}</p>
+          </div>
+          <div className="flex gap-4 cursor-pointer items-center">
+            <Link href={`/post/${post.id}`}>
+              <p className="text-sm font-bold text-gray-700">
+                Comments ({post.comments.length})
+              </p>
+            </Link>
+          </div>
+        </div>
       ))}
     </>
   );
